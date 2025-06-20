a64675d232c68883fd1cbf5261814a5f874e7d37
Joy677129 <onezeronineone@gmail.com>
2025-06-20 16:19:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ except ImportError:
 VIDEO_LIST = [
     {"path": "videos/a(intS)b.mp4", "title": "n(AUB)=n(A)+n(B)-n(A∩B)"},{"path": "videos/a(int)bUC.mp4", "title": "A∩(B∪C)=(A∩B)∪(A∩C)"},
 
-     {"path": "videos/aub_vid.mp4", "title": "A∩B"},
+     #{"path": "videos/aub_vid.mp4", "title": "A∩B"},
 ]
 
 # --- Streamlit App: Set Operations, Power Set, Slideshow & Video Viewer, Set Definition Validator ---
